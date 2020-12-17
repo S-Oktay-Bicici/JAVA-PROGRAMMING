4e1a6b9c8fe62e53244ceb384dcaf0f8dc7c81bf
Sadık Oktay Biçici <s.oktay.bicici@gmail.com>
2020-12-18 02:07:11 +0300

    Add files via upload

---
+++ 11.0.Object-Oriented-Programming(OOP)/11.1-Encapsulation(Kapsülleme)/Otomobil.java
@@ -0,0 +1,25 @@
+/*
+private olarak tanımlanan özelliklere get ve set metodları
+ilr erişim sağlayarak okuma ve değiştrime işlemleri yapıyoruz.
+*/
+
+public class Otomobil{
+	private Sring marka;
+	private int model;
+	
+	public Sring getMarka(){
+		rerurn marka;
+	}
+	
+	public void setMarka(int marka){
+		this.marka = marka;
+	}
+	
+	public int getModel(){
+		return model;
+	}
+	
+	public void setModel(int model){
+		this.model = model;
+	}
+}
